42c3fe854687b5b4a7e1e4dd679bdc5ba4ae8eb3
singhrishabhj <singhrishabhj19@gmail.com>
2024-12-21 16:18:48 +0530

    Update the REPL functionality  in the program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,24 @@ public class Main {
 	
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+	
+	while(true){ //if the condition is true
+
+                //checks if the input is exit to break the loop
+                if(input.equals("exit")){
+                        System.out.println("Program exited"); // we can change it further
+                        break;//exit 
+                }
+	//the command not found will print
         System.out.println(input + ": command not found");
+
+	//then again the "$" printed , that command is not correct reneter it 
+        System.out.print("$ ");
         
+	//again ask for another input
+	input = scanner.nextLine();
+	
+	
+	}
     }
 }
