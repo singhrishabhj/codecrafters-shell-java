16e9849341a097aa67f26d676a335a9cc04a4b0b
singhrishabhj <singhrishabhj19@gmail.com>
2024-12-21 12:11:21 +0530

    Uncomment code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        // System.out.print("$ ");
-
+        //System.out.print("$ ");
+	System.out.println("$ ");
+	
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
     }
