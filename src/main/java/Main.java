8b977d7ed6bf883b7e5e95ad4b40701422f1a4b2
singhrishabhj <singhrishabhj19@gmail.com>
2024-12-21 12:57:54 +0530

    Updated the input+ cmd not found

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println(input + ": command not found");
+        
     }
 }
